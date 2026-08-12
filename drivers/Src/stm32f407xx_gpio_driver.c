124ad94e30fccfedb4ad28de484ad6752b162a70
Robinhoets <robertets88@gmail.com>
2026-08-12 17:23:07 -0400

    GPIO_Init built. TODO interrupt mode

--- drivers/Src/stm32f407xx_gpio_driver.c
+++ drivers/Src/stm32f407xx_gpio_driver.c
@@ -122,15 +122,57 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
  */
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 {
-	// (1) Configure the mode of the gpio pin
+	uint32_t temp = 0;
+
+	// (1) Configure the mode of the GPIO pin
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		// each pin is two bits. Left shift the pin mode value by 2 * pin number. Ex) 11 << 2 * 1
+		temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ) );
+		// clear the register
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+		// store value in actual register
+		pGPIOHandle->pGPIOx->MODER |= temp;
+	}
+	else
+	{
+		// In interrupt mode - TODO code later
+	}
+
+	temp = 0;
 
 	// (2) Configure the speed
+	temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ) );
+	// clear the register
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;
+	temp = 0;
 
 	// (3) configure the pull up and pull down settings
+	temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ) );
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->PUPDR |= temp;
+	temp = 0;
 
 	// (4) Configure the output type
+	temp = ( pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber );
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->OTYPER |= temp;
+	temp = 0;
 
 	// (5) Configure the alternate functionality
+	/*
+	 * 	Alternate function only works when pin mode is set to alternate function.
+	 * 	in GPIO_RegDef_t data structure, we represent the low with AFR[0] and high with AFR[1]
+	 */
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		uint8_t temp1, temp2;
+		temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;
+		temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8;
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~( 0xF << ( 4 * temp2 ) );
+		pGPIOHandle->pGPIOx->AFR[temp1] |= ( pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << ( 4 * temp2 ) );
+	}
 }
 
 /***************************************************************************
